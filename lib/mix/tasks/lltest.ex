f0b1b091f9143a4fe5a790dcc0c648876550a43a
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-10-04 17:10:24 +0200

    add moduledoc

--- lib/mix/tasks/lltest.ex
+++ lib/mix/tasks/lltest.ex
@@ -1,6 +1,49 @@
 defmodule Mix.Tasks.Lltest do
   @shortdoc "Run LL-HLS stress test"
-  @moduledoc "TODO writeme"
+  @moduledoc """
+  # Name
+
+  `mix lltest` - #{@shortdoc}
+
+  # Synopsis
+
+  ```
+  mix lltest [--url <url>] [--clients <count>] [--time <seconds>]
+             [--spawn-interval <milliseconds>] [--out-path <path>]
+             [--jellyfish-address <address>] [--jellyfish-token <token>] [--secure]
+  ```
+
+  # Description
+
+  Mix task for running stress-tests on a Jellyfish serving LL-HLS.
+
+  This tool primarily tests the load handling capability and performance
+  of the media server. The test simulates multiple clients requesting LL-HLS
+  content streams concurrently over a specified duration.
+
+  It saves a CSV file with test results after the full duration of the test has passed,
+  which means that if the test is interrupted e.g. using Ctrl-C, the results will NOT be saved.
+
+  # Available options
+
+  * `--url <url>` - URL of the master HLS manifest. This can be inferred from Jellyfish, see below
+  * `--clients <count>` - Number of client connections will to simulate. Defaults to 500
+  * `--time <seconds>` - Duration of the test. Defaults to 300 seconds
+  * `--spawn-interval <milliseconds>` - Interval at which to spawn new clients. Defaults to 200 milliseconds
+  * `--out-path <path>` - Path to store the CSV with test results. Defaults to "results.csv"
+
+  If `--url <url>` is not passed, the tool will attempt to infer the URL by communicating with Jellyfish.
+  This uses the following options:
+
+  * `--jellyfish-address <address>` - Address (structured `<host>:<port>`) of Jellyfish. Defaults to "localhost:5002"
+  * `--jellyfish-token <token>` - Jellyfish token. Defaults to "development"
+  * `--secure` - By default, the tool will try to communicate with Jellyfish using HTTP.
+  If this option is passed, it will use HTTPS instead
+
+  # Example command
+
+  `mix lltest --jellyfish-address my-jellyfish.org:443 --jellyfish-token my-token --secure --clients 2000 --time 600`
+  """
 
   use Mix.Task
   alias Jellygrinder.Coordinator
